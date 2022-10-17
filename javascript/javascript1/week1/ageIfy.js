2beefbc14693fc2a70cf0403e4fe220674385363
farzanaIslam <farzanaislam2188@gmail.com>
2022-10-17 19:13:43 +0200

    house price calculation done

---
+++ javascript/javascript1/week1/ageIfy.js
@@ -0,0 +1,5 @@
+let yearOfBirth =1990;
+let yearFuture=2027;
+console.log(" year of birth : "+typeof yearOfBirth +" year future birth :"+ typeof yearFuture);
+let age=yearFuture-yearOfBirth;
+console.log("you will be "+age+"years old in 2027")
---
+++ javascript/javascript1/week1/dogAge.js
@@ -0,0 +1,10 @@
+let dogYearOfBirth=2017;
+let dogYearFuture =2027;
+let dogYear=dogYearFuture-dogYearOfBirth;
+let shouldShowResultInDogYears=false;
+if(shouldShowResultInDogYears){
+    console.log( "your dog will be "+dogYear+" human years old in 2027")
+}
+else{
+    console.log( "your dog will be "+dogYear*7+" human years old in 2027")
+}
