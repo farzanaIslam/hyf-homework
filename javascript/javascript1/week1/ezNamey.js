512bbed281fabf6b25e7c1ac4e2868de12682da5
farzanaIslam <farzanaislam2188@gmail.com>
2022-10-18 10:13:23 +0200

    randomly any array index pick up and added two array data in ezNamy.js file

---
+++ javascript/javascript1/week1/ezNamey.js
@@ -0,0 +1,13 @@
+const firstWords=["Easy", "Awesome", "Corporate","paradise","support","connect","nethub","nova","strong","decca"];
+const secondWords=["like","long","cute","cube","insta","mine","high","quickly","active","correct"];
+const randomNumber = Math.floor(Math.random() * firstWords.length);
+ let startupName= firstWords[randomNumber]+" "+secondWords[randomNumber];
+ console.log("The startup: "+startupName+" contains "+startupName.length+" characters")
+      
+
+   
+   
+    
+    
+
+
